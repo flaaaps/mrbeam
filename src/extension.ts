40d1e4e77aeb9cf95341b1e29bb29a07234c9b67
flaaaps <julianwening4@gmail.com>
2021-04-21 22:05:25 +0200

    Get webhook from settings

--- src/extension.ts
+++ src/extension.ts
@@ -10,31 +10,36 @@ export function activate(context: vscode.ExtensionContext) {
 }
 
 async function registerSubmitBean() {
-    const hook = new webhook.Webhook(
-        "https://discord.com/api/webhooks/834507248813211699/-OITLyn1eF6wp5ejhW6FWy46o5WZLe_X_mmEKUcVtslbyEZ0_I25DnFi8Qr9RQJMnqWI"
-    )
-    const filePath = vscode.window.activeTextEditor?.document.fileName
-    const editor = vscode.window.activeTextEditor
-    let cursorPosition = editor?.selection.start
-    let selected = editor?.document.getText(editor.selection)
-    if (!selected) {
-        console.log("Nothing selected")
+    const discordWebhookUrl: string | undefined = vscode.workspace
+        .getConfiguration("misterbeam")
+        .get("discordWebhookUrl")
+    if (!discordWebhookUrl) {
+        vscode.window.showInformationMessage("Please specify a webhook url.")
     } else {
-        if (cursorPosition) {
-            if (filePath) {
-                console.log(filePath)
-                console.log(path.basename(filePath))
-                const fileName = path.basename(filePath)
-                const ext = fileName.split(".")[fileName.split(".").length - 1]
-                const msg = new webhook.MessageBuilder().setName("Mister Beam").setText(`\`\`\`${ext}\n
+        const hook = new webhook.Webhook(discordWebhookUrl)
+        const filePath = vscode.window.activeTextEditor?.document.fileName
+        const editor = vscode.window.activeTextEditor
+        let cursorPosition = editor?.selection.start
+        let selected = editor?.document.getText(editor.selection)
+        if (!selected) {
+            console.log("Nothing selected")
+        } else {
+            if (cursorPosition) {
+                if (filePath) {
+                    console.log(filePath)
+                    console.log(path.basename(filePath))
+                    const fileName = path.basename(filePath)
+                    const ext = fileName.split(".")[fileName.split(".").length - 1]
+                    const msg = new webhook.MessageBuilder().setName("Mister Beam").setText(`\`\`\`${ext}\n
 ${selected}
 \`\`\``)
-                await hook.send(msg)
+                    await hook.send(msg)
+                }
             }
         }
-    }
 
-    vscode.window.showInformationMessage("Beamed it up!")
+        vscode.window.showInformationMessage("Beamed it up!")
+    }
 }
 
 export function deactivate() {}
