212a5c83f33f464b8012acc6afcdd9670e49f0c0
Julian Wening <julian@wening.me>
2022-01-21 23:30:45 +0100

    Cleanup code

--- src/extension.ts
+++ src/extension.ts
@@ -15,44 +15,46 @@ async function registerSubmitBean() {
     const discordWebhookUrl: string | undefined = config.get("discordWebhookUrl")
     const userImageUrl: string | undefined = config.get("imageUrl")
     const username: string | undefined = config.get("username")
+
     if (!discordWebhookUrl) {
         vscode.window.showInformationMessage("Please specify a webhook url.")
-    } else {
-        const hook = new webhook.Webhook(discordWebhookUrl)
-        const filePath = vscode.window.activeTextEditor?.document.fileName
-        const editor = vscode.window.activeTextEditor
-        let cursorPosition = editor?.selection.start
-        let selected = editor?.document.getText(editor.selection)
-        if (!selected) {
-            console.log("Nothing selected")
-            vscode.window.showInformationMessage("Select some code to beam up.")
-        } else {
-            if (cursorPosition && filePath) {
-                console.log(filePath)
-                console.log(path.basename(filePath))
-                const fileName = path.basename(filePath)
-                let ext = fileName.split(".")[fileName.split(".").length - 1]
-                if (ext === "tsx") ext = "ts"
-                else if (ext === "jsx") ext = "js"
-                const msg = new webhook.MessageBuilder()
-                    .setName("Mister Beam")
-                    .setAuthor(username ?? "User", userImageUrl)
-                    .setTitle(fileName)
-                    .setDescription(
-                        `\`\`\`${ext}\n
+        return
+    }
+
+    const editor = vscode.window.activeTextEditor
+    const hook = new webhook.Webhook(discordWebhookUrl)
+
+    let selected = editor?.document.getText(editor.selection)
+    if (!selected) {
+        vscode.window.showInformationMessage("Select some code to beam up.")
+        return
+    }
+
+    const filePath = vscode.window.activeTextEditor?.document.fileName
+    let cursorPosition = editor?.selection.start
+    if (!cursorPosition || !filePath) {
+        vscode.window.showErrorMessage("Could not upload message.")
+        return
+    }
+
+    const fileName = path.basename(filePath)
+
+    let ext = fileName.split(".")[fileName.split(".").length - 1]
+    if (ext === "tsx") ext = "ts"
+    else if (ext === "jsx") ext = "js"
+
+    const msg = new webhook.MessageBuilder()
+        .setName("Mister Beam")
+        .setAuthor(username ?? "User", userImageUrl)
+        .setTitle(fileName)
+        .setDescription(
+            `\`\`\`${ext}\n
 ${selected}
 \`\`\``
-                    )
-                    .setFooter("Made with ❤️ by flaaaps", "https://content.wening.me/mrbeam/logos/logo-white.png")
-                    .setTime(Date.now() / 1000)
+        )
+        .setFooter("Made with ❤️ by flaaaps", "https://content.wening.me/mrbeam/logos/logo-white.png")
+        .setTime(Date.now() / 1000)
 
-                await hook.send(msg)
-                vscode.window.showInformationMessage("Beamed it up!")
-            }
-        }
-    }
-}
-
-export function deactivate() {
-    console.log("Disabling extension")
+    await hook.send(msg)
+    vscode.window.showInformationMessage("Message successfully beamed to discord.")
 }
